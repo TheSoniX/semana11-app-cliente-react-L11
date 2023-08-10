6184e2fe977eb5b2b60dc13ee3aa6f8636305c67
TheSoniX <sonic_4499@hotmail.com>
2023-08-10 23:44:21 +0000

    finalizando request POST-GET OK

--- src/main/java/com/example/demo/DatabaseLoader.java
+++ src/main/java/com/example/demo/DatabaseLoader.java
@@ -7,17 +7,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class DatabaseLoader implements CommandLineRunner {
 
-	private final InstrumentoRepository repository;
+	private final InstrumentoRepository repositoryI;
+	private final MusicoRepository repositoryM;
 
 	@Autowired
-	public DatabaseLoader(InstrumentoRepository repository) {
-		this.repository = repository;
+	public DatabaseLoader(InstrumentoRepository repositoryI, MusicoRepository repositoryM) {
+		this.repositoryI = repositoryI;
+		this.repositoryM = repositoryM;
 	}
 
 	@Override
 	public void run(String... strings) throws Exception {
-		this.repository.save(new Instrumento("Guitarra", "Cuerda", "de madera, con caja de resonancia, 6 cuerdas temp."));
-		this.repository.save(new Instrumento("Ukelele", "Cuerda", "de madera, con caja de resonancia"));
-		this.repository.save(new Instrumento("Melodica", "Viento", "teclado pequeño de 2 octavas"));
+		this.repositoryI.save(new Instrumento("Guitarra", "Cuerda", "de madera, con caja de resonancia, 6 cuerdas temp."));
+		this.repositoryI.save(new Instrumento("Ukelele", "Cuerda", "de madera, con caja de resonancia"));
+		this.repositoryI.save(new Instrumento("Melodica", "Viento", "teclado pequeño de 2 octavas"));
+		this.repositoryM.save(new Musico("Daniel F"));
 	}
 }
